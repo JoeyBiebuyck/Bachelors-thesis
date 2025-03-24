16833e6b4847a0fee38b807e99cbe486bfbe58b5
joe-oui <joey.biebuyck@gmail.com>
2025-03-24 19:00:02 +0100

    copied most of bfts into my project because I could not get imports to work (I do not claim this work, I just need it to make my bandit)

---
+++ XSS_bandit.py
@@ -0,0 +1,9 @@
+# import requests
+
+# r = requests.get('http://127.0.0.1:8000/')
+
+# print(r.text)
+
+import sys
+sys.path.append(r"D:\Documents\Programming projects\CLONED PROJECTS\bfts")
+from bandit import Bandit
