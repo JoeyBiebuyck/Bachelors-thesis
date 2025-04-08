61867a95136921e3c0c09ee86c8e874042a0daf4
joe-oui <joey.biebuyck@gmail.com>
2025-04-08 20:32:49 +0200

    updated all views to respond with HTTP 200 or 404

--- safari_website/homepage/views.py
+++ safari_website/homepage/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+import random
 
 def homepage(request):
     template = loader.get_template('home.html')
@@ -9,4 +10,11 @@ def homepage(request):
 def search_view(request):
     query = request.GET.get('q', '')  # Get the search term from the URL
     # TODO: here we can add code about what to do with a specific query, eg. sampling from a binomial distribution and returning success or failure
-    return render(request, 'search.html', {'query': query})
+
+    # currently returning a random succes/failure
+    if random.randint(0, 1) == 0:
+        return HttpResponse(status=200)
+    else:
+        return HttpResponse(status=404)
+
+    # return render(request, 'search.html', {'query': query}) # uncomment this if you are manually testing whether an evasion technique works
