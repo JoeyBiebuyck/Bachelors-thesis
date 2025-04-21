9395501883147ca38ad5c82f0bf40ab8655ce11f
joe-oui <joey.biebuyck@gmail.com>
2025-04-21 18:30:19 +0200

    fixed small typo/bug

--- run_atlucb_xss.py
+++ run_atlucb_xss.py
@@ -9,7 +9,7 @@ from bfts.algorithms.atlucb import AT_LUCB
 from bfts.algorithms.uniform import Uniform
 from bfts.run_utils import print_header, run
 
-# n_arms = 20
+n_arms = 20 # MUST MANUALLY UPDATE EACH views.py FILE
 
 parser = ArgumentParser(description="XSS ATLUCB")
 
@@ -21,7 +21,7 @@ parser.add_argument("-e", "--environment", dest="env", type=str, required=False)
 
 args = parser.parse_args()
 
-n_arms = args.arms
+#n_arms = args.arms
 
 np.random.seed(args.seed)
 bandit = XSS_bandit(n_arms)
