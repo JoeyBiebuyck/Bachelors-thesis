ec69287b2e5ccf86a1380c4fc6039776da38c588
joe-oui <joey.biebuyck@gmail.com>
2025-04-23 16:01:04 +0200

    implemented run_bernoulli_bandit

---
+++ run_atlucb_bernoulli.py
@@ -0,0 +1,25 @@
+import numpy as np
+from argparse import ArgumentParser
+
+from Bernoulli_bandit import Bernoulli_bandit
+from bfts.algorithms.atlucb import AT_LUCB 
+from bfts.run_utils import print_header,run
+
+parser = ArgumentParser(description="ATLUCB")
+
+parser.add_argument("-s", "--seed", dest="seed", type=int, required=True)
+parser.add_argument("-t", "--time", dest="time", type=int, required=True)
+parser.add_argument("-m", "--m", dest="m",type=int, required=True)
+
+args = parser.parse_args()
+
+np.random.seed(args.seed)
+
+bandit = Bernoulli_bandit(10)
+
+print_header(args.m)
+sigma=0.5
+alpha=0.99
+epsilon=0
+algo = AT_LUCB(bandit, args.m, sigma, alpha, epsilon)
+run(algo, args.time)
