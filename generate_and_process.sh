05876ee61cd17c07c08eca3ee826b52174d154a3
joe-oui <joey.biebuyck@gmail.com>
2025-04-27 12:39:58 +0200

    added merge_and_plot.py to the generate_and_process.sh script

--- generate_and_process.sh
+++ generate_and_process.sh
@@ -4,8 +4,10 @@ dir="results"
 
 for r in {1..100}
 do
-python run_atlucb_xss.py -s $r -t 100 -m 2 > $dir/atlucb.$r.csv
+python run_atlucb_xss.py -s $r -t 10000 -m 2 > $dir/atlucb.$r.csv
 python postprocess.py -m 2 -c $dir/atlucb.$r.csv -s prop_of_success > "$dir/atlucb-$r.prop_of_success"
-python run_uniform_xss.py -s $r -t 200 -m 2 > $dir/uniform.$r.csv
+python run_uniform_xss.py -s $r -t 10000 -m 2 > $dir/uniform.$r.csv
 python postprocess.py -m 2 -c $dir/uniform.$r.csv -s prop_of_success > "$dir/uniform-$r.prop_of_success"
 done
+
+python merge_and_plot.py
