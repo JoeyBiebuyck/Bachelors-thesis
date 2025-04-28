da67b4799d659e851b13a85db4cb7dad03cbdb61
joe-oui <joey.biebuyck@gmail.com>
2025-04-28 22:27:39 +0200

    added prints

--- bfts/run_utils.py
+++ bfts/run_utils.py
@@ -6,4 +6,7 @@ def run(algo, steps):
     for t in range(1, steps + 1):
         J_t = algo.step(t)
         J_t = [str(i) for i in J_t]
+        # print(f"Current means of all the arms: {algo.mean_per_arm}")
+        # arm_with_mean = list(zip(range(0, 20), algo.mean_per_arm)) # MOET JE AANPASSEN ALS JE MEER ARMS WIL
+        # print(f"This is each arm with its respective mean: {arm_with_mean}")
         print(str(t) + "," + ",".join(J_t), flush=True)
