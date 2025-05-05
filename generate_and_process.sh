4e55469d275603f49f06c790b0f8ea6bba47a7b4
Joey <your-username@users.noreply.github.com>
2025-05-05 10:54:38 +0200

    experiment of best techniques

--- generate_and_process.sh
+++ generate_and_process.sh
@@ -5,7 +5,7 @@ batch_start_time=$(date +%s)
 total_time=0
 
 amount_of_timesteps=150
-n_arms=13 # dont forget to change this value in all the files!
+n_arms=20 # dont forget to change this value in all the files!
 m_top=2
 experiment_name="${n_arms}arms_${amount_of_timesteps}t_${m_top}m_PlusEpsilon_03-05-2025"
 
