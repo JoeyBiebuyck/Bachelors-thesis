703db7f7a9d1c076c068a45a3eb6e81e410d7448
joe-oui <joey.biebuyck@gmail.com>
2025-05-07 19:12:41 +0200

    specified which arms can infect which websites

--- websites/website_2/homepage/views.py
+++ websites/website_2/homepage/views.py
@@ -4,8 +4,9 @@ from django.template import loader
 import random
 #from run_atlucb_xss import n_arms
 
-n_arms = 13
-disarm_rate = 0.25
+# n_arms = 13
+# disarm_rate = 0.50
+strongestDisarmedInput = 2
 
 def homepage(request):
     template = loader.get_template('home.html')
@@ -16,8 +17,8 @@ def search_view(request):
 
     # What to do with a specific query, eg. sampling from a binomial distribution and returning success or failure
     # could also do the below filtering by putting a < in the if clause
-    strongestDisarmedInput = int(n_arms * disarm_rate)
-    disarmedInputs = [str(x) for x in list(range(1, strongestDisarmedInput+1))] # the weakest 25% (5) of the XSS payloads get sanitized by this website's filter (there are 20 transformations possible)
+    # strongestDisarmedInput = int(n_arms * disarm_rate)
+    disarmedInputs = [str(x) for x in list(range(1, strongestDisarmedInput+1))] # the weakest 50% (10) of the XSS payloads get sanitized by this website's filter (there are 20 transformations possible)
 
     # returning succes/failure
     if query in disarmedInputs:
